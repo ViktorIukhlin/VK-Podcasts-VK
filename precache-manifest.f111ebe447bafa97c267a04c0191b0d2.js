00620bca9c30b09fc9a121b774d8fe89f3893907
Viktor Iukhlin <iukhlin.v@gmail.com>
2020-09-18 10:47:37 +0300

    Updates

---
+++ precache-manifest.f111ebe447bafa97c267a04c0191b0d2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bfe66f0e89b41aeaf54f6aeaf61bff9",
+    "url": "/VK-Podcasts-VK/index.html"
+  },
+  {
+    "revision": "d6b7ce9ae660e9aeb80f",
+    "url": "/VK-Podcasts-VK/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "c52f6cb9bb0985f3de21",
+    "url": "/VK-Podcasts-VK/static/css/main.8c44ca8b.chunk.css"
+  },
+  {
+    "revision": "d6b7ce9ae660e9aeb80f",
+    "url": "/VK-Podcasts-VK/static/js/2.298dc009.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/VK-Podcasts-VK/static/js/2.298dc009.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c52f6cb9bb0985f3de21",
+    "url": "/VK-Podcasts-VK/static/js/main.6f8f24be.chunk.js"
+  },
+  {
+    "revision": "38f72337ceb813257b2e",
+    "url": "/VK-Podcasts-VK/static/js/runtime-main.f257363d.js"
+  },
+  {
+    "revision": "2361d339bce4c4153bbfe8ee3e1042e7",
+    "url": "/VK-Podcasts-VK/static/media/papadog.2361d339.svg"
+  },
+  {
+    "revision": "2331c4c421c3248480256db67a6fb718",
+    "url": "/VK-Podcasts-VK/static/media/zag.2331c4c4.svg"
+  }
+]);
